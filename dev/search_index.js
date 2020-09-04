512f92e4707d5372a574b403075a1329e76ba574
Documenter.jl <documenter@juliadocs.github.io>
2020-09-04 23:17:04 +0000

    build based on 2feedcc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SossMLJExamples","category":"page"},{"location":"#SossMLJExamples","page":"Home","title":"SossMLJExamples","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This repository contains additional examples for the  SossMLJ package.","category":"page"}]
+}
